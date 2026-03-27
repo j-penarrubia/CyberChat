ff6b97f8b0e22ad27f9e7e0c5d13bdcdbacd554c
j-penarrubia <penarrubia.92@gmail.com>
2026-03-27 13:56:28 +0100

    [CHNG]Strict en cookies

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(session({
     cookie: {
         maxAge: 1000 * 60 * 60 * 24,
         secure: false,//process.env.NODE_ENV === 'production',
-        sameSite: 'Lax'
+        sameSite: 'Strict'
     }
 }));
 
