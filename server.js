d805272e349ad4e73e94c0d746ed13904fd99cea
j-penarrubia <penarrubia.92@gmail.com>
2026-03-27 12:16:25 +0100

    [CHNG]Cambio para probar en render

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(session({
     }),
     cookie: {
         maxAge: 1000 * 60 * 60 * 24,
-        secure: process.env.NODE_ENV === 'production',
+        secure: false,//process.env.NODE_ENV === 'production',
         sameSite: 'Strict'
     }
 }));
